cd471fd1bc4ee3ac9a127abd32e05ae8625e728d
WhAASD LQPD <myslicky@ya.ru>
2026-07-20 23:27:02 +0300

    Implement Contact and PhoneBook classes with methods for contact management, validation, and JSON storage

---
+++ 6/web_phone/phonebook.py
@@ -0,0 +1,59 @@
+import re
+import json
+from datetime import datetime
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+CONTACTS_FILE = 'contacts.json'
+
+class Contact:
+    def __init__(self, name : str, phone : str, updated_on : str):
+        self.name = name
+        self.phone = phone
+        self.updated_on = updated_on
+
+    def __str__(self) -> str:
+        return f"Имя: {self.name}, Номер телефона: {self.phone}, Последнее обновление: {self.updated_on}"
+    
+    def update_phone(self, new_phone):
+        self.phone = new_phone
+        self.updated_on = datetime.now().isoformat
+
+    @staticmethod
+    def is_valid_phone(phone: str) -> bool:
+        pattern = r'^\d{1}-\d{3}-\d{3}-\d{2}-\d{2}$'
+        return re.match(pattern, phone) is not None
+
+class PhoneBook:
+    def __init__(self):
+        self.contacts = {}
+    
+    def __str__(self):
+        return "class phonebook"
+    
+    def load(self, filename=CONTACTS_FILE):
+        if os.path.exists(filename):
+            with open(filename, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                self.contacts = {name: Contact(name, item['phone'], item['updated_on']) for name, item in data.items()}
+
+    def save(self, filename=CONTACTS_FILE):
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(name: contact.__dict__ for name, contact in self.contacts.items(), f, ensure_ascii=False, indent=2)
+
+    def add(self, name: str, phone: str)
+        if not self.find(name):
+            if not Contact.is_valid_phone(phone):
+                raise ValueError('неверный формат номера телефона')
+            self.contacts[name] = Contact(name=name, phone=phone, datetime.now().isoformat())
+            self.save()
+            return True
+        return False
+    
+    def delete(self, name: str) -> bool:
+        if name in self.contacts:
+            del self.contacts[name]
+            self.save()
+            return True
+        return False
