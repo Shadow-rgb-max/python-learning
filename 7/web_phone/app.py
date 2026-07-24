e7a18fb77d4e53ff48d9dd4ebabd0100edba4d0e
WhAASD LQPD <myslicky@ya.ru>
2026-07-24 23:13:12 +0300

    Add migration setup and initial database schema for user and contact models

--- 7/web_phone/app.py
+++ 7/web_phone/app.py
@@ -4,23 +4,28 @@ from routes import phonebook_bp
 from auth import auth_bp
 from flask_login import LoginManager
 
+from flask_migrate import Migrate
+
+migrate = Migrate()
+
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
+app.config['SECRET_KEY'] = 'любая-случайная-строка-держи-в-секрете'
+db.init_app(app)
+migrate.init_app(app, db)
+login_manager.init_app(app)
+
 
 @login_manager.user_loader
 def load_user(user_id):
     from phonebook import User
     return db.session.get(User, int(user_id))
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
-app.config['SECRET_KEY'] = 'любая-случайная-строка-держи-в-секрете'
-db.init_app(app)
-login_manager.init_app(app)
 app.register_blueprint(phonebook_bp)
 app.register_blueprint(auth_bp)
-with app.app_context():
-    db.create_all()
 
 if __name__ == '__main__':
     app.run()
